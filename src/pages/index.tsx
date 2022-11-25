a5f2f23cd4c3c0fb6dbb7f078bf3ef8c364bb85d
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-11-26 01:07:01 +0900

    fix: style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,17 @@ const Page: NextPage = () => {
     <>
       <MyPageSeo path='/' noTitleTemplate={true} noindex={true} />
       <section className='mx-auto bg-n-50 py-12 text-center'>
-        <h1 className='mb-6 text-4xl font-bold'>議論&ナレッジ共有プラットフォーム</h1>
-        <img
-          className='mx-auto'
-          alt='付箋を使ってミーティングを女性二人と男性2人のフラットイラスト'
-          src='/mtg.png'
-          height='450'
-          width='450'
-        ></img>
-        <p className='text-lg text-gray-500'>{config.siteMeta.description}</p>
+        <div className='mx-auto max-w-screen-md px-4 md:px-8'>
+          <h1 className='mb-6 text-4xl font-bold'>議論&ナレッジ共有プラットフォーム</h1>
+          <img
+            className='mx-auto'
+            alt='付箋を使ってミーティングを女性二人と男性2人のフラットイラスト'
+            src='/mtg.png'
+            height='450'
+            width='450'
+          ></img>
+          <p className='text-lg text-gray-500'>{config.siteMeta.description}</p>
+        </div>
       </section>
     </>
   )
