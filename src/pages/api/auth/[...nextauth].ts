0e38ae4538b0cf3d32f7bf14386e13fd4add5183
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-01-14 04:19:03 +0900

    :recycle: rename

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,13 +10,14 @@ export default NextAuth({
       return true // Do different verification for other providers that don't have email_verified
     },
   },
+  debug: process.env.VERCEL_ENV ? false : true,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   theme: {
     colorScheme: "auto",
   },
