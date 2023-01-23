da439c7252d00e05d901f12cf4f6c428edf481b7
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-01-23 15:43:01 +0900

    ♻️ refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,14 @@ const Page: NextPage = () => {
   if (session && session.user) {
     return (
       <>
-        <MyPageSeo path="/" title={"ホーム"} noindex={true} />
+        <MyPageSeo path="/" title="ホーム" />
         <User />
       </>
     )
   }
   return (
     <>
-      <MyPageSeo
-        path="/"
-        title={"NJR Feedback | 議論&ナレッジ共有プラットフォーム"}
-        noTitleTemplate={true}
-      />
+      <MyPageSeo path="/" title="NJR Feedback | 議論&ナレッジ共有プラットフォーム" />
       <section className="mx-auto bg-n-50 py-12 text-center">
         <div className="mx-auto max-w-screen-md px-4 md:px-8">
           <h1 className="mb-6 text-4xl font-bold">議論&ナレッジ共有プラットフォーム</h1>
@@ -39,18 +35,18 @@ const Page: NextPage = () => {
           </p>
           <button
             onClick={() => signIn("google")}
-            className="inline-block items-center justify-center rounded-lg bg-white px-12 py-3 text-center text-xl font-bold text-gray-700 shadow"
+            className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-12 py-3 text-center font-inter text-xl font-bold text-gray-700 shadow-md shadow-gray-300"
           >
             <span className="mr-2 inline-flex items-center">
-              <img src="/google.png" alt="Slack" width="18" height="18"></img>
+              <img src="/google.svg" alt="Slack" width="22" height="22"></img>
             </span>
-            Sign in With Google
+            Login With Google
           </button>
           <p className="mt-4 text-base text-gray-500">
             ( <Link href="/guideline">ガイドライン</Link>
             に同意の上、<span className="font-medium">@n-jr.jp</span>
             または<span className="font-medium">@nnn.ac.jp</span>
-            を含むGoogleアカウントでサインインしてください )
+            を含むGoogleアカウントでログインしてください )
           </p>
         </div>
       </section>
