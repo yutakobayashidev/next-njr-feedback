6821f333429c9255e1e2f9c76108024584da2117
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-01-30 18:44:06 +0900

    🐛 fix api error

--- src/pages/api/knowledge/index.ts
+++ src/pages/api/knowledge/index.ts
@@ -24,78 +24,77 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
       error: { code: 500, messsage: "サーバーがセッションユーザーIDの取得に失敗しました" },
     })
 
-  switch (req.method) {
-    case HttpMethod.POST:
-      const emojiRegex = initEmojiRegex()
-      const matches = pickRandomEmoji().match(emojiRegex)
+  if (req.method === HttpMethod.POST) {
+    const emojiRegex = initEmojiRegex()
+    const matches = pickRandomEmoji().match(emojiRegex)
 
-      if (!matches || !matches[0] || matches[1])
-        return res.status(401).json({
-          message: "リクエストパラメータに不足・不備がある可能性があります。",
+    if (!matches || !matches[0] || matches[1])
+      return res.status(401).json({
+        message: "リクエストパラメータに不足・不備がある可能性があります。",
+      })
+
+    try {
+      const result = await prisma.knowledge.create({
+        data: {
+          contributors: { connect: { id: session.user.id } },
+          course: { connect: { id: 1 } },
+          creator: { connect: { id: session.user.id } },
+          emoji: pickRandomEmoji(),
+        },
+      })
+      res.status(201).json(result)
+    } catch (error) {
+      console.error(error)
+      return res.status(500).end(error)
+    }
+  } else if (req.method === HttpMethod.GET) {
+    const querySchema = z.object({
+      count: z
+        .string()
+        .refine((v) => {
+          return !isNaN(Number(v))
         })
+        .transform((v) => Number(v)),
+    })
 
-      try {
-        const result = await prisma.knowledge.create({
-          data: {
-            contributors: { connect: { id: session.user.id } },
-            course: { connect: { id: 1 } },
-            creator: { connect: { id: session.user.id } },
-            emoji: pickRandomEmoji(),
-          },
-        })
-        res.status(201).json(result)
-      } catch (error) {
-        console.error(error)
-        return res.status(500).end(error)
-      }
-    case HttpMethod.GET:
-      const querySchema = z.object({
-        count: z
-          .string()
-          .refine((v) => {
-            return !isNaN(Number(v))
-          })
-          .transform((v) => Number(v)),
-      })
+    const result = querySchema.safeParse(req.query)
 
-      const result = querySchema.safeParse(req.query)
+    if (!result.success) {
+      return res.status(400).json({ error: { messsage: "クエリが不正です" } })
+    }
 
-      if (!result.success) {
-        return res.status(400).json({ error: { messsage: "クエリが不正です" } })
-      }
+    const { count } = result.data
 
-      const { count } = result.data
-
-      const data = await prisma.knowledge.findMany({
-        include: {
-          contributors: {
-            select: {
-              name: true,
-              image: true,
-            },
+    const data = await prisma.knowledge.findMany({
+      include: {
+        contributors: {
+          select: {
+            name: true,
+            image: true,
           },
         },
-        orderBy: [
-          {
-            updatedAt: "desc",
-          },
-        ],
-        take: count,
-        where: {
-          archive: false,
-          published: true,
+      },
+      orderBy: [
+        {
+          updatedAt: "desc",
         },
-      })
+      ],
+      take: count,
+      where: {
+        archive: false,
+        published: true,
+      },
+    })
 
-      const knowledge = JSON.parse(JSON.stringify(data))
+    const knowledge = JSON.parse(JSON.stringify(data))
 
-      res.status(201).json(knowledge)
-    default:
-      return res.status(400).json({
-        error: {
-          code: 400,
-          messsage: `${req.method}メソッドはサポートされていません。`,
-        },
-      })
+    res.status(201).json(knowledge)
+  } else {
+    return res.status(400).json({
+      error: {
+        code: 400,
+        messsage: `${req.method}メソッドはサポートされていません。`,
+      },
+    })
   }
 }
