5f49965c7f88f0bac13888b62d59add067a601cc
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-02-12 18:07:09 +0900

    🐛 fix api error message

--- src/pages/api/knowledge/[id]/views.ts
+++ src/pages/api/knowledge/[id]/views.ts
@@ -13,7 +13,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     return res.status(400).json({
       error: {
         code: 400,
-        messsage: `ディスカッションIDまたはセッションが見つかりませんでした`,
+        messsage: `ナレッジIDまたはセッションが見つかりませんでした`,
       },
     })
   }
