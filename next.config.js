144a8a8ea033ed585df8f67c36c21e912ae27822
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-02-13 09:20:40 +0900

    ✨use svgr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    disableStaticImages: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -1,3 +1,4 @@
+import Google from "@public/google.svg"
 import { signIn } from "next-auth/react"
 
 export const GoogleLogin: React.FC<{ callbackUrl?: string }> = ({ callbackUrl }) => {
@@ -8,7 +9,7 @@ export const GoogleLogin: React.FC<{ callbackUrl?: string }> = ({ callbackUrl })
         className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-12 py-3 text-center font-inter text-base font-bold text-gray-700 shadow-md shadow-gray-300"
       >
         <span className="mr-2 inline-flex items-center">
-          <img src="/google.svg" alt="Google" width="18" height="18" />
+          <Google width={18} height={18} />
         </span>
         Login With Google
       </button>
