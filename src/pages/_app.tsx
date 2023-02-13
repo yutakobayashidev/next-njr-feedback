be3d9c5fc9af7b5fbba5062d105f04bc47baa84a
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-02-13 20:26:58 +0900

    ✨ add easteregg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@src/styles/globals.css" // Tailwind CSS
 
 import { Inter } from "@next/font/google"
+import { config } from "@site.config"
 import GoogleAnalytics from "@src/components/GoogleAnalytics"
 import usePageView from "@src/hooks/usePageView"
 import { NextPage } from "next"
@@ -23,6 +24,12 @@ export default function App({ Component, pageProps }: AppPropsWithLayout<{ sessi
   const getLayout = Component.getLayout || ((page: any) => page)
 
   usePageView()
+
+  var style = "color:#0099D9; font-size:3em"
+  console.log(
+    `%c${config.siteMeta.title}の開発に貢献することに興味がありますか？\n${config.siteMeta.repository}`,
+    style,
+  )
 
   return (
     <>
