29f9742633cf680b914c2076a4a19b5b66f2d6fe
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-02-16 16:29:32 +0900

    💄 fix style

--- src/components/DiscussionSideber.tsx
+++ src/components/DiscussionSideber.tsx
@@ -23,7 +23,7 @@ export const CommentSidebar: React.FC<{
 
   return (
     <>
-      <div className="flex-1 p-4">
+      <div className="flex-1">
         {props.user.id == session.user.id && (
           <>
             {archived ? (
