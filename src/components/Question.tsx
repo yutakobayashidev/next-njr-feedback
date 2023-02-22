76b030577dcf506d697017270604926ee5ef2c4e
poppo <taku.mabuchi0825@gmail.com>
2023-02-22 11:33:37 +0900

    archived to resolved

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -31,7 +31,7 @@ export const QuestionCard: React.FC<{ question: QuestionProps }> = ({ question }
           </Link>
           <div className="mt-2 flex items-center text-xs md:text-base">
             <span className="mr-2 rounded-2xl bg-coursebg px-3 py-1 font-bold text-course">
-              {question.archive ? "Archive" : "Open"}
+              {question.archive ? "Resolved" : "Open"}
             </span>
             <Link
               href={getUserpagePath(question.user.handle)}
