25ca736fb47e01e3bbc9b224fdb5e08d2e7d44e7
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-02-26 00:39:10 +0900

    🥚 niconico

---
+++ src/pages/niconico.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Page: NextPage = () => {
+  return (
+    <a href="https://twitter.com/sigekun/status/1526078995598831618">
+      <img src="/niconico.gif" alt="niconico" />
+    </a>
+  )
+}
+
+export default Page
